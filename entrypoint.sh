3f8023febf5d375adb44050700014160d043902f
Nikolai Kolodziej <7687617+kldzj@users.noreply.github.com>
2022-01-27 02:18:07 +0100

    remove IMAGE_NAME, PULL_IMAGE, extract ref using git

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
 DOCKER_TOKEN=$1
-DOCKER_IMAGE_NAME=$2
-DOCKER_IMAGE_TAG=$3
-EXTRACT_TAG_FROM_GIT_REF=$4
-DOCKERFILE=$5
-BUILD_CONTEXT=$6
-PULL_IMAGE=$7
-CUSTOM_DOCKER_BUILD_ARGS=$8
+DOCKER_IMAGE_TAG=$2
+EXTRACT_TAG_FROM_GIT_REF=$3
+DOCKERFILE=$4
+BUILD_CONTEXT=$5
+CUSTOM_DOCKER_BUILD_ARGS=$6
 
 if [ $EXTRACT_TAG_FROM_GIT_REF == "true" ]; then
-  DOCKER_IMAGE_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
+  DOCKER_IMAGE_TAG=$(git rev-parse --short HEAD)
 fi
 
-DOCKER_IMAGE_NAME=$(echo ghcr.io/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME} | tr '[:upper:]' '[:lower:]')
+DOCKER_IMAGE_NAME=$(echo ghcr.io/${GITHUB_REPOSITORY} | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} | tr '[:upper:]' '[:lower:]')
 
 docker login -u publisher -p ${DOCKER_TOKEN} ghcr.io
-
-if [ $PULL_IMAGE == "true" ]; then
-  docker pull $DOCKER_IMAGE_NAME_WITH_TAG || docker pull $DOCKER_IMAGE_NAME || 1
-fi
 
 docker build -t $DOCKER_IMAGE_NAME_WITH_TAG -f $DOCKERFILE $CUSTOM_DOCKER_BUILD_ARGS $BUILD_CONTEXT
 docker push $DOCKER_IMAGE_NAME_WITH_TAG
