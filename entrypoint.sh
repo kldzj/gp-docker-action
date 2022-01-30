4a06b8da79eda47aba02d8328a90308433a93b04
kldzj <github@kldzj.dev>
2022-01-30 11:22:34 +0100

    only build once

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DOCKER_TOKEN=$1
 DOCKER_IMAGE_TAG=$2
@@ -9,25 +9,22 @@ BUILD_CONTEXT=$6
 CUSTOM_DOCKER_BUILD_ARGS=$7
 
 TAGS=()
-
 if [ -n "$DOCKER_IMAGE_TAG" ]; then
   TAGS+=(DOCKER_IMAGE_TAG)
 fi
 
-if [ $EXTRACT_SHA_FROM_COMMIT == "true" ]; then
-  TAGS+=($(git rev-parse --short HEAD))
+if [ "$EXTRACT_SHA_FROM_COMMIT" == "true" ]; then
+  TAGS+=("$(git rev-parse --short HEAD)")
 fi
 
-if [ $EXTRACT_TAG_FROM_GIT_REF == "true" ]; then
-  TAGS+=($(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g"))
+if [ "$EXTRACT_TAG_FROM_GIT_REF" == "true" ]; then
+  TAGS+=("$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g")")
 fi
 
-docker login -u publisher -p ${DOCKER_TOKEN} ghcr.io
-
+TAG_FLAGS=()
 DOCKER_IMAGE_NAME="ghcr.io/${GITHUB_REPOSITORY}"
-for tag in ${TAGS[*]}
-do
-  DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${tag} | tr '[:upper:]' '[:lower:]')
-  docker build -t $DOCKER_IMAGE_NAME_WITH_TAG -f $DOCKERFILE $CUSTOM_DOCKER_BUILD_ARGS $BUILD_CONTEXT
-  docker push $DOCKER_IMAGE_NAME_WITH_TAG
-done
+for tag in "${TAGS[@]}"; do TAG_FLAGS+=(-f "$DOCKER_IMAGE_NAME:$tag"); done
+
+docker login -u publisher -p "${DOCKER_TOKEN}" ghcr.io
+docker build "${TAG_FLAGS[@]}" -f "$DOCKERFILE" $CUSTOM_DOCKER_BUILD_ARGS "$BUILD_CONTEXT"
+docker push "$DOCKER_IMAGE_NAME" --all-tags
