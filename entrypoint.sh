3a0aff738ee6b1aab531f252aa60da187ecb1949
kldzj <github@kldzj.dev>
2022-05-29 23:10:29 +0200

    add logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,20 +10,25 @@ CUSTOM_DOCKER_BUILD_ARGS=$7
 
 TAGS=()
 if [ -n "$DOCKER_IMAGE_TAG" ]; then
+  echo "Fixed tag: $DOCKER_IMAGE_TAG"
   TAGS+=("$DOCKER_IMAGE_TAG")
 fi
 
 if [ "$EXTRACT_TAG_FROM_COMMIT_SHA" == "true" ]; then
-  TAGS+=("$(git rev-parse --short HEAD)")
+  EXTRACTED_TAG=$(git rev-parse --short HEAD)
+  echo "Extracted tag from commit sha: $EXTRACTED_TAG"
+  TAGS+=("$EXTRACTED_TAG")
 fi
 
 if [ "$EXTRACT_TAG_FROM_GIT_REF" == "true" ]; then
-  TAGS+=("$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g")")
+  EXTRACTED_TAG="$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g")"
+  echo "Extracted tag from git ref: $EXTRACTED_TAG"
+  TAGS+=("$EXTRACTED_TAG")
 fi
 
 TAG_FLAGS=()
 DOCKER_IMAGE_NAME="ghcr.io/${GITHUB_REPOSITORY}"
-for tag in "${TAGS[@]}"; do TAG_FLAGS+=(-t "$DOCKER_IMAGE_NAME:$tag"); done
+for tag in "${TAGS[@]}"; do TAG_FLAGS+=("-t $DOCKER_IMAGE_NAME:$tag"); done
 
 docker login -u publisher -p "${DOCKER_TOKEN}" ghcr.io
 docker build "${TAG_FLAGS[@]}" -f "$DOCKERFILE" $CUSTOM_DOCKER_BUILD_ARGS "$BUILD_CONTEXT"
